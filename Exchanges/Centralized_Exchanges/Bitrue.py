a4db030dbfcf6c0bd6080612668885d9e3c2fa43
Kelvin Leong Zhin Sheng <zhinsheng97@hotmail.com>
2022-04-14 13:55:50 +0800

    Code Refactoring

--- Exchanges/Centralized_Exchanges/Bitrue.py
+++ Exchanges/Centralized_Exchanges/Bitrue.py
@@ -1,11 +1,11 @@
 import requests
-from typing import Tuple
 
 class Bitrue:
+    
     def __init__(self) -> None:
         self.__api = "https://www.bitrue.com/exchange-web/web/tokenInfo/full?coinName={tokenSymbol}&language=en&appName=Netscape&appCodeName=Mozilla&appVersion=5.0+(Windows+NT+10.0%3B+Win64%3B+x64)+AppleWebKit%2F537.36+(KHTML,+like+Gecko)+Chrome%2F100.0.4896.60+Safari%2F537.36&userAgent=Mozilla%2F5.0+(Windows+NT+10.0%3B+Win64%3B+x64)+AppleWebKit%2F537.36+(KHTML,+like+Gecko)+Chrome%2F100.0.4896.60+Safari%2F537.36&cookieEnabled=true&platform=Win32&userLanguage=en-US&vendor=Google+Inc.&onLine=true&product=Gecko&productSub=20030107&mimeTypesLen=4&pluginsLen=3&javaEnbled=false&windowScreenWidth=1920&windowScreenHeight=1080&windowColorDepth=24&bitrueLanguage=en_US&token="
 
-    def GetWebsiteUrl(self,tokenSymbol:str, maxRetries:int=6) -> Tuple[str,str]:
+    def GetWebsiteUrl(self,tokenSymbol:str, maxRetries:int=6) -> str:
         api = self.__api.format(tokenSymbol=tokenSymbol)
         websiteUrl = -1
         retries = 0
